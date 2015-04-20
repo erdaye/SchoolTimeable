fe90af17d51a9810e9db03204afcae60e745e0a2
erdaye <1186108562@qq.com>
2015-04-20 09:59:58 +0800

    idea 做了更改
    原来想用开心网的框架来完成搭配帮，可是加了下边栏就耗费了很长一段时间，所以打算放弃这个想法，做比较简单的应用，课程格子。

---
+++ KaiXin/src/com/kaixin/android/activity/home_popular.java
@@ -0,0 +1,15 @@
+package com.kaixin.android.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class home_popular extends Activity {
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		
+	}
+
+}
